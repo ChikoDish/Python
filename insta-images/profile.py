0034b18fd8c9f7e147c3808488be98599d67f170
Ankit Kumar Gupta <ankitkumargupta00@gmail.com>
2021-09-23 21:16:21 +0530

    instagram images

--- insta-images/profile.py
+++ insta-images/profile.py
@@ -1,4 +1,4 @@
 import instaloader
 bot1 = instaloader.Instaloader()
-username = 'sassy.varsha'
-print(bot1.download_profile(username))
+username = '' #insta username
+print(bot1.download_profile(username),profile_pic_only=True)
