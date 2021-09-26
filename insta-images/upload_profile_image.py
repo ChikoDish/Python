dcc60005307509619b24ea9ee6f78682188e183c
Ankit Kumar Gupta <ankitkumargupta00@gmail.com>
2021-09-26 10:54:55 +0530

    Instagram

--- insta-images/upload_profile_image.py
+++
@@ -1,6 +0,0 @@
-from instabot import Bot
-bot = Bot()
-user_name = ''
-user_password = ''
-bot.login(username = user_name, password = user_password)
-bot.upload_photo("profile.png", caption="Pic from Python Program")
