19ca47c3487a1c521496b663d7ef70d6c37b3a9b
Ankit Kumar Gupta <ankitkumargupta00@gmail.com>
2021-10-01 21:47:05 +0530

    new patterns

--- patterns/star_1.py
+++ patterns/star_1.py
@@ -1,8 +1,23 @@
-
 def make_pattern(n):
     for i in range(1,n+1):
         for j in range(i):
             print('* ', end="")
         print("\r")
 
+def make_number_pyramid(n):
+    for i in range(1,n+1):
+        for j in range(i):
+            print(i, end="")
+        print("\r")
+
+
+def make_number_pyramid1(n):
+    for i in range(1, n+1):
+        for j in range(1,i+1):
+            print(j, end="")
+        print("\r")
+
 make_pattern(5)
+make_number_pyramid(5)
+make_number_pyramid1(5)
+
