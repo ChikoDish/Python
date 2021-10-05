a1d6619dc8d1bf994e5587be54e074639303ad31
Ankit Kumar Gupta <ankitkumargupta00@gmail.com>
2021-10-05 20:59:23 +0530

    new pattern

--- patterns/star_1.py
+++ patterns/star_1.py
@@ -16,8 +16,15 @@ def make_number_pyramid1(n):
         for j in range(1,i+1):
             print(j, end="")
         print("\r")
+        
 
-make_pattern(5)
-make_number_pyramid(5)
-make_number_pyramid1(5)
+
+def make_reverse_pattern(n):
+    for i in range(n, 0, -1):
+        print((n-i) * ' ' + i * '*')
+
+# make_pattern(5)
+# make_number_pyramid(5)
+# make_number_pyramid1(5)
+make_reverse_pattern(5)
 
