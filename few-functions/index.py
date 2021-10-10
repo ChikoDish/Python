a9f8df4636d326dece9c1d21b286528a5eea4f16
Ankit Kumar Gupta <ankitkumargupta00@gmail.com>
2021-10-10 08:57:26 +0530

    change function

--- few-functions/index.py
+++ few-functions/index.py
@@ -1,4 +1,4 @@
-def leat_distance(a,b,c):
+def least_distance(a,b,c):
     numbers = sorted([a,b,c])
     a = numbers[0]
     b = numbers[1]
@@ -6,13 +6,7 @@ def leat_distance(a,b,c):
     deff1 = abs(c - b)
     deff2 = abs(c - a)
     deff3 = abs(b - a)
-    print(deff1 , deff2 , deff3)
-    if deff1 < deff2 and deff1 < deff3:
-        return deff1
-    elif deff1 > deff2 and deff2 < deff3:
-        return deff2
-    else:
-        return deff3
+    return min(deff1, deff2, deff3)
     
-print(leat_distance(13,19,25))
+print(least_distance(1,19,5))
     
