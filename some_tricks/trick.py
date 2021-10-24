94eeaf0bb5160ac7d65f7be1c7ee7e508eb7e9aa
Ankit Kumar Gupta <ankitkumargupta00@gmail.com>
2021-10-24 12:50:12 +0530

    some python tricks

--- some_tricks/trick.py
+++ some_tricks/trick.py
@@ -1,4 +1,5 @@
 import sys
+from typing_extensions import Concatenate
 
 # multiple inputs in a single line is
 # x,y,z = input('Enter number: ').split()
@@ -9,10 +10,15 @@ import sys
 
 #list vs generators
 
-my_list = [ i for i in range(2000)]
-print(sum(my_list))
-print(sys.getsizeof(my_list))
+# my_list = [ i for i in range(2000)]
+# print(sum(my_list))
+# print(sys.getsizeof(my_list))
 
-my_generator = (i for i in range(2000))
-print(sum(my_generator))
-print(sys.getsizeof(my_generator))
+# my_generator = (i for i in range(2000))
+# print(sum(my_generator))
+# print(sys.getsizeof(my_generator))
+
+
+arr = ['d','d','e','o']
+concatenate = ''.join(arr)
+print(concatenate)
