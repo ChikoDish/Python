3d2e41dd4e8537188f50b360346a006f20d5fc50
Ankit Kumar Gupta <ankitkumargupta00@gmail.com>
2021-10-24 12:41:36 +0530

    some python tricks

--- some_tricks/trick.py
+++ some_tricks/trick.py
@@ -1,5 +1,14 @@
+import sys
+
 # multiple inputs in a single line is
-x,y,z = input('Enter number: ').split()
-print(x)
-print(y)
-print(z)
+# x,y,z = input('Enter number: ').split()
+# print(x)
+# print(y)
+# print(z)
+
+
+#list vs generators
+
+my_list = [ i for i in range(2000)]
+print(sum(my_list))
+print(sys.getsizeof(my_list))
